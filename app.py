5e870a8cb37675276f85c9926f3c530c0cc265c3
Garubieru <gabrielmorishita23@gmail.com>
2022-10-19 16:14:47 -0300

    chore: add env variables

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from factories.RemoteCurrencyConverterFactory import create_remote_currency_converter
 from modules.CurrencyConverterController import CurrencyConverterController
 from modules.Server import Server
+from dotenv import dotenv_values
+
+config = dotenv_values()
 
 if __name__ == "__main__":
     server = Server()
     server.add_route('/convertemoeda/<amount>',
                      'convertemoeda', CurrencyConverterController(create_remote_currency_converter()))
-    server.listen(5000)
+    server.listen(config['PORT'], config['HOST'])
--- modules/Server.py
+++ modules/Server.py
@@ -7,9 +7,9 @@ class Server:
     def __init__(self) -> None:
         self.app: Flask = Flask(__name__)
 
-    def listen(self, port: int):
+    def listen(self, port: int, host='localhost'):
         self.port = port
-        self.app.run(port=port)
+        self.app.run(port=port, host=host)
 
     def add_route(self, endpoint: str, endpoint_name: str, controller: Controller):
         self.app.add_url_rule(
